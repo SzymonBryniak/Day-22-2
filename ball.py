e02b6f717c0beaa19ea587cd7d7c3a2c1d483014
SzymonBryniak <szymonbryniakproject@gmail.com>
2024-07-28 19:13:24 +0100

    lesson 204. Ball stops when it reaches the top wall.
    Signed-off-by: SzymonBryniak <szymonbryniakproject@gmail.com>

--- ball.py
+++ ball.py
@@ -12,5 +12,16 @@ class Ball(Turtle):
     def move_ball(self):
         new_x = self.xcor() + 10
         new_y = self.ycor() + 10
-        # self.speed(1)
+        if new_x - 5 >= 290:
+            return
+        else:
+            self.goto(new_x, new_y)
+            print(self.xcor())
+
+    def move_ball_udemy(self):
+        new_x = self.xcor() + 10
+        new_y = self.ycor() + 10
         self.goto(new_x, new_y)
+        return new_x, new_y
+
+
